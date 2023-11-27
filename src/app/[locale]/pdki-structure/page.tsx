74e6b815a653d75811524772223c92bc32a38d7a
adenkesuma <adenfdfd10@gmail.com>
2023-11-27 09:25:08 +0700

    `feat:` add dropdown navigate

--- src/app/[locale]/pdki-structure/page.tsx
+++ src/app/[locale]/pdki-structure/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import Image from "next/image"
+
+import Navbar from "@/components/navbar"
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 
 const PDKIStructure = () => {
   return (
-    <div>PDKIStructure</div>
+      <div>
+      <Navbar />
+      <main>
+        <Header />
+        
+        <div className="lg:mt-12 mt-8">
+          {/* <Image 
+            src="/"
+            alt="pdki struktur"
+            width={1000}
+            height={1000}
+          /> */}
+
+          <h2 className="text-2xl font-bold text-gray-800 h-40 text-center">
+            Struktur Organisasi Belum Tersedia
+          </h2>
+        </div>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,35 +1,41 @@
-import { FC } from "react"
+"use client"
+
+import { FC, useState } from "react"
 import Link from "next/link"
 
 const Navigation: FC = () => {
+  const [pdki, setPdki] = useState<boolean>(false)
+  const [member, setMember] = useState<boolean>(false)
 
   return (
-    <nav className="container mx-auto rounded-tl-xl rounded-tr-xl bg-[#274698] py-4 px-4 sm:px-8 lg:px-12 h-10 md:h-14">
+    <nav className="relative container mx-auto rounded-tl-xl rounded-tr-xl bg-[#274698] py-4 px-4 sm:px-8 lg:px-12">
       <ul className="flex items-center sm:justify-center gap-6 lg:gap-8 xl:gap-12 snap overflow-scroll sm:overflow-auto">
         <li>
           <Link href="/" className="text-xs text-white font-semibold hover:underline delay-100">
             PDKI
           </Link>
         </li>
+
         <li>
-          <select className="bg-transparent outline-none border-none text-white text-xs font-semibold hover:underline delay-100">
-            <option value="pdki-structure" className="text-sm text-gray-800 font-semibold py-6 border-none">
-              <Link href="/pdki-structure">
+          <button onClick={() => setPdki(!pdki)} className="font-semibold text-white rounded-lg text-xs text-center inline-flex items-center" type="button">Tentang PDKI
+            <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg>
+          </button>
+
+          {pdki && (
+            <div className={`absolute left-[14%] top-12 p-3 flex flex-col gap-3 z-50 rounded-lg bg-[#274698] border border-gray-200`}>
+              <Link href="/about" className="text-xs text-white font-semibold hover:bg-[rgba(255,255,255,0.2)] py-2 px-4 rounded-lg">
+                Tentang PDKI
+              </Link>
+              <Link href="/pdki-structure" className="text-xs text-white font-semibold hover:bg-[rgba(255,255,255,0.2)] py-2 px-4 rounded-lg">
                 Struktur PDKI
               </Link>
-            </option>
-            <option value="about" className="text-sm text-gray-800 font-semibold py-6 border-none">
-              <Link href="/about">
-                About
-              </Link>
-            </option>
-          </select>
+            </div>
+          )}
         </li>
-        <li>
-          <Link href="/facilities" className="text-xs text-white font-semibold hover:underline delay-100">
-            Fasilitas
-          </Link>
-        </li>
+
+
         <li>
           <Link href="/news" className="text-xs text-white font-semibold hover:underline delay-100">
             Berita
@@ -40,28 +46,34 @@ const Navigation: FC = () => {
             Konferensi
           </Link>
         </li>
+
         <li>
-          <select className="bg-transparent outline-none border-none text-white text-xs font-semibold hover:underline delay-100">
-            <option value="register-member" className="text-sm text-gray-800 font-semibold py-6 border-none">
-              <Link href="/register-member">
+          <button onClick={() => setMember(!member)} className="font-semibold text-white rounded-lg text-xs text-center inline-flex items-center" type="button">Anggota PDKI
+            <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg>
+          </button>
+
+          {member && (
+            <div className={`absolute left-[38%] top-12 p-3 flex flex-col gap-3 z-50 rounded-lg bg-[#274698] border border-gray-200`}>
+              <Link href="https://docs.google.com/forms/d/e/1FAIpQLSemnLw1xbr01SWjsbWZTcYYcPoif77a8LKsG40yIGiO-gLDZQ/viewform?fbzx=7610966734699982512" className="text-xs text-white font-semibold hover:bg-[rgba(255,255,255,0.2)] py-2 px-4 rounded-lg">
                 Daftar Anggota PDKI
               </Link>
-            </option>
-            <option value="member" className="text-sm text-gray-800 font-semibold py-6 border-none">
-              <Link href="/member/copc">
+              <Link href="/member/copc" className="text-xs text-white font-semibold hover:bg-[rgba(255,255,255,0.2)] py-2 px-4 rounded-lg">
                 Anggota PDKI
               </Link>
-            </option>
-          </select>
+            </div>
+          )}
         </li>
+
         <li>
-          <Link href="https://l2-kb-lms.vercel.app/" className="text-xs text-white font-semibold hover:underline delay-100">
+          <Link href="https://ldplayananprimer.pdkindonesia.com/" className="text-xs text-white font-semibold hover:underline delay-100">
             P2KB 
           </Link>
         </li>
         <li>
           <Link href="/dedication" className="text-xs text-white font-semibold hover:underline delay-100">
-            Pengabdian Masyarakat
+            <p>Pengabdian Masyarakat</p>
           </Link>
         </li>
         <li>
